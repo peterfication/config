f3c8785852976d606f43e728242430754caa7be0
Peter Gundel <mail@petergundel.de>
2021-03-03 18:14:11 +0100

    Fix ts command for dirs with dots

--- zsh/tmux.zsh
+++ zsh/tmux.zsh
@@ -2,14 +2,14 @@ setopt shwordsplit
 
 function tmux_new_with_name() {
   for TS_DIR in $1; do
-    DIR_NAME=`echo $TS_DIR | grep -o "[a-zA-Z0-9_-]*$"`
+    DIR_NAME=`echo $TS_DIR | grep -o "[a-zA-Z0-9_.-]*$" | sed -r 's/[.]+/-/g'`
     eval "tmux new-session -d -c ${TS_DIR} -s $DIR_NAME -n main"
   done
 }
 
 function ts() {
   tmux_new_with_name `pwd`
-  DIR_NAME=`pwd | grep -o "[a-zA-Z0-9_-]*$"`
+  DIR_NAME=`pwd | grep -o "[a-zA-Z0-9._-]*$" | sed -r 's/[.]+/-/g'`
   tmux -2 a -t $DIR_NAME
 }
 
