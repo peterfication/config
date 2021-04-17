f23c64e885ab3601cf2543b079c3e1dc06ac96a8
Peter Gundel <mail@petergundel.de>
2021-04-17 07:47:03 +0200

    [ZSH] Add pr-checkout command

--- zsh/07_git.zsh
+++ zsh/07_git.zsh
@@ -69,3 +69,37 @@ export GIT_FUZZY_STATUS_DISCARD_KEY="Ctrl-U"
 #
 # # log internal commands (pretty noisy)
 # export GF_INTERNAL_COMMAND_DEBUG_MODE="YES"
+
+##
+# Use FZF to select and checkout a PR with the help
+# of the Github CLI
+#
+# From https://seb.jambor.dev/posts/improving-shell-workflows-with-fzf/
+function pr-checkout() {
+  local jq_template pr_number
+
+  jq_template='"'\
+'#\(.number) - \(.user.login) - \(.title)'\
+'\t'\
+'Author: \(.user.login)\n'\
+'Created: \(.created_at)\n'\
+'Updated: \(.updated_at)\n\n'\
+'\(.body)'\
+'"'
+
+  pr_number=$(
+    gh api 'repos/:owner/:repo/pulls' |
+    jq ".[] | $jq_template" |
+    sed -e 's/"\(.*\)"/\1/' -e 's/\\t/\t/' |
+    fzf \
+      --with-nth=1 \
+      --delimiter='\t' \
+      --preview='echo -e {2}' \
+      --preview-window=top:wrap |
+    sed 's/^#\([0-9]\+\).*/\1/'
+  )
+
+  if [ -n "$pr_number" ]; then
+    gh pr checkout "$pr_number"
+  fi
+}
