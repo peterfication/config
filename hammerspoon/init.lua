52460a1d1b6366274b993e7059c51188b3c28624
Peter Gundel <mail@petergundel.de>
2022-01-09 19:09:58 +0200

    Add ClipboardTool to Hammerspoon

--- hammerspoon/init.lua
+++ hammerspoon/init.lua
@@ -12,6 +12,17 @@ sky = SkyRocket:new({
   resizeModifiers = {'ctrl', 'shift'},
 })
 
+local ClipboardTool = hs.loadSpoon("ClipboardTool")
+ClipboardTool.show_in_menubar = false
+ClipboardTool.paste_on_select = true
+ClipboardTool:start()
+ClipboardTool:bindHotkeys({
+  toggle_clipboard = {{'cmd', 'shift'}, 'V'},
+})
+
 hs.hotkey.bind({"cmd", "alt", "ctrl"}, "W", function()
   hs.alert.show("Hello World!")
 end)
+
+-- # TODO
+-- https://zzamboni.org/post/my-hammerspoon-configuration-with-commentary/
