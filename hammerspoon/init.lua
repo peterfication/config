0e401ca4fbd36c925cd4e1a1086efad36f7bb0c0
Peter Gundel <mail@petergundel.de>
2022-01-09 18:50:07 +0200

    Add SkyRocket to Hammerspoon

--- hammerspoon/init.lua
+++ hammerspoon/init.lua
@@ -6,6 +6,12 @@ hs.loadSpoon("Caffeine")
 spoon.Caffeine:start()
 spoon.Caffeine:setState(true)
 
+local SkyRocket = hs.loadSpoon("SkyRocket")
+sky = SkyRocket:new({
+  moveModifiers = {'cmd', 'shift'},
+  resizeModifiers = {'ctrl', 'shift'},
+})
+
 hs.hotkey.bind({"cmd", "alt", "ctrl"}, "W", function()
   hs.alert.show("Hello World!")
 end)
