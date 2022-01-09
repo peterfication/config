d17b8892c20455e3817b67c679a99b7dc5274987
Peter Gundel <mail@petergundel.de>
2022-01-09 20:46:05 +0200

    Add FnMate to Hammerspoon

--- hammerspoon/init.lua
+++ hammerspoon/init.lua
@@ -2,6 +2,9 @@ hs.loadSpoon("ReloadConfiguration")
 spoon.ReloadConfiguration:start()
 hs.alert.show("Config loaded")
 
+-- Vim movements
+hs.loadSpoon("FnMate")
+
 hs.loadSpoon("Caffeine")
 spoon.Caffeine:start()
 spoon.Caffeine:setState(true)
