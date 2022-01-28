8bf3e52f344f233cad3d607dd0375693afe78a11
Peter Gundel <mail@petergundel.de>
2022-01-28 20:07:55 +0100

    Dont show alert in ClipboardTool

--- hammerspoon/init.lua
+++ hammerspoon/init.lua
@@ -17,6 +17,7 @@ sky = SkyRocket:new({
 
 local ClipboardTool = hs.loadSpoon("ClipboardTool")
 ClipboardTool.show_in_menubar = false
+ClipboardTool.show_copied_alert = false
 ClipboardTool.paste_on_select = true
 ClipboardTool:start()
 ClipboardTool:bindHotkeys({
