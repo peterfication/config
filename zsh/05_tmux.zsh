40a7e21ef49ad16add6ab2fd7bbd939f0841151a
Peter Gundel <mail@petergundel.de>
2022-03-12 19:52:42 +0100

    Adjust ts command to current needs

--- zsh/05_tmux.zsh
+++ zsh/05_tmux.zsh
@@ -17,13 +17,17 @@ function tmux_new_with_name() {
   for TS_DIR in $1; do
     DIR_NAME=`echo $TS_DIR | grep -o "[a-zA-Z0-9_.-]*$" | sed -r 's/[.]+/-/g'`
     echo "Create session '${DIR_NAME}' with window 'vim' and folder '${TS_DIR}'"
-    eval "tmux new-session -d -c ${TS_DIR} -s ${DIR_NAME} -n console"
+    eval "tmux new-session -d -c ${TS_DIR} -s ${DIR_NAME} -n vim"
+    eval "tmux send-keys -t 1.0 vim Enter"
 
-    echo "Split window 'console' in session '${DIR_NAME}'"
-    eval "tmux split-window -h -t ${DIR_NAME} -c ${TS_DIR}"
+    # echo "Split window 'console' in session '${DIR_NAME}'"
+    # eval "tmux split-window -h -t ${DIR_NAME} -c ${TS_DIR}"
 
-    echo "Create window 'vim' in session '${DIR_NAME}' with folder '${TS_DIR}'"
-    eval "tmux new-window -t ${DIR_NAME}: -c ${TS_DIR} -n vim 'vim'"
+    echo "Create window 'console' in session '${DIR_NAME}' with folder '${TS_DIR}'"
+    eval "tmux new-window -t ${DIR_NAME}: -c ${TS_DIR} -n console"
+
+    # Show vim window
+    eval "tmux next-window"
   done
 }
 
