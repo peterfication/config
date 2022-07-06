19515aac8ab46d253b800436998dbf3e272c16e1
Peter Gundel <mail@petergundel.de>
2022-07-06 14:46:54 +0200

    Add fzf script loading from new homebrew location

--- zsh/09_other.zsh
+++ zsh/09_other.zsh
@@ -7,7 +7,11 @@
 # The ZVM plugin will auto execute this zvm_after_init function.
 # The addiotional sources are needed to restore behavior after ZVM overwrote it.
 function zvm_after_init() {
+  [ -f /opt/homebrew/opt/fzf/shell/completion.zsh ] && source /opt/homebrew/opt/fzf/shell/completion.zsh
+  [ -f /opt/homebrew/opt/fzf/shell/key-bindings.zsh ] && source /opt/homebrew/opt/fzf/shell/key-bindings.zsh
+
   [ -f /usr/local/opt/fzf/shell/completion.zsh ] && source /usr/local/opt/fzf/shell/completion.zsh
   [ -f /usr/local/opt/fzf/shell/key-bindings.zsh ] && source /usr/local/opt/fzf/shell/key-bindings.zsh
+
   [ -f ~/.zplug/repos/Aloxaf/fzf-tab/fzf-tab.plugin.zsh ] && source ~/.zplug/repos/Aloxaf/fzf-tab/fzf-tab.plugin.zsh
 }
