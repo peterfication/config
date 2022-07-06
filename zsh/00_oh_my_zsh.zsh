f908868ebb36baeff126fa8b225302fc9e41e788
Peter Gundel <mail@petergundel.de>
2022-07-06 14:48:12 +0200

    Use zplug version of oh my zsh

--- zsh/00_oh_my_zsh.zsh
+++ zsh/00_oh_my_zsh.zsh
@@ -1,5 +1,6 @@
 # Path to your oh-my-zsh installation.
-export ZSH=~/.oh-my-zsh
+# export ZSH=~/.oh-my-zsh
+export ZSH=~/.zplug/repos/robbyrussell/oh-my-zsh
 
 # Set name of the theme to load.
 # Look in ~/.oh-my-zsh/themes/
