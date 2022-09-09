143b7dded501d154243adab6251084b6138484f8
Peter Gundel <mail@petergundel.de>
2022-09-09 15:44:35 +0200

    Adjust Ruby installation on M1

--- install/10_ruby.sh
+++ install/10_ruby.sh
@@ -31,10 +31,24 @@ then
   echo "Ruby 2.7.2 is already installed. Skipping."
 else
   echo "Installing Ruby 2.7.2 ..."
-  rvm install ruby-2.7.2
-fi
+  # From https://nickymarino.com/2021/12/17/install-ruby-273-on-m1/
+  brew install openssl@1.1
 
-rvm alias create default 2.7.2
+  export PATH="$(brew --prefix)/opt/openssl@1.1/bin:$PATH"
+  export LDFLAGS="-L$(brew --prefix)/opt/openssl@1.1/lib"
+  export CPPFLAGS="-I$(brew --prefix)/opt/openssl@1.1/include"
+  export PKG_CONFIG_PATH="$(brew --prefix)/opt/openssl@1.1/lib/pkgconfig"
+
+  rvm autolibs disable
+  rvm pkg install readline
+
+  export RUBY_CFLAGS=-DUSE_FFI_CLOSURE_ALLOC
+  export optflags="-Wno-error=implicit-function-declaration"
+
+  rvm install 2.7.5 --with-openssl-dir=$(brew --prefix)/opt/openssl@1.1 --with-readline-dir=$(brew --prefix)/opt/readline
+  rvm install 2.7.3 --with-openssl-dir=$(brew --prefix)/opt/openssl@1.1 --with-readline-dir=$(brew --prefix)/opt/readline
+  rvm install 2.5.8 --with-openssl-dir=$(brew --prefix)/opt/openssl@1.1 --with-readline-dir=$(brew --prefix)/opt/readline
+fi
 
 echo
 echo "Installing gems ..."
