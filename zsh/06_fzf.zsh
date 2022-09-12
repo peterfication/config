a99b0a65b8136c92395d9206ace104725f0ebbe8
Peter Gundel <mail@petergundel.de>
2022-09-12 12:20:36 +0200

    Add FZF shell functions/alias for files

--- zsh/06_fzf.zsh
+++ zsh/06_fzf.zsh
@@ -17,7 +17,7 @@ rga-fzf() {
 # From https://medium.com/@sidneyliebrand/how-fzf-and-ripgrep-improved-my-workflow-61c7ca212861
 # mnemonic: [K]ill [P]rocess
 kp() {
-  local pid=$(ps -ef | sed 1d | eval "fzf ${FZF_DEFAULT_OPTS} -m --header='[kill:process]'" | awk '{print $2}')
+  local pid=$(ps -ef | sed 1d | eval "fzf  -m --header='[kill:process]'" | awk '{print $2}')
 
   if [ "x$pid" != "x" ]
   then
@@ -32,3 +32,22 @@ find-command() {
 }
 
 alias fzrepl="~/config/zsh/fzf/fzrepl.sh"
+
+# Preview files in current dir with FZF like in my Vim settings <leader>e
+alias pf="fzf --preview='less {}' --bind shift-up:preview-page-up,shift-down:preview-page-down"
+alias preview-files=pf
+
+# Use FZF to navigate with cd
+function fcd() {
+    local dir;
+
+    while true; do
+      # exit with ^D
+      dir="$(ls -a1F | grep '[/@]$' | grep -v '^./$' | sed 's/@$//' | fzf --height 40% --reverse --no-multi --preview 'pwd' --preview-window=up,1,border-none --no-info)"
+      if [[ -z "${dir}" ]]; then
+        break
+      elif [[ -d "${dir}" ]]; then
+        cd "${dir}"
+      fi
+    done
+  }
