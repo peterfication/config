8e67d344335b0ca76cdff09d6e9286121839b4cf
Peter Gundel <mail@petergundel.de>
2022-10-10 16:50:33 +0200

    Add config reload to vimlua

--- vim-lua/maps.lua
+++ vim-lua/maps.lua
@@ -6,3 +6,6 @@ vim.g.mapleader = ' '
 
 options = { noremap = true }
 map('n', '<Leader><ESC>', ':nohlsearch<CR>', options)
+
+-- Reload the vim-lua configuration
+map('n', '<Leader>ü', ':source ~/config/vim-lua/init.lua<CR>', options)
