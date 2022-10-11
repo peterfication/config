b708482c0c77a4b19512fabfac6dfc3ad554335d
Peter Gundel <mail@petergundel.de>
2022-10-11 17:45:24 +0200

    Add more Telescope mappings in vim lua

--- vim-lua/config/keymaps.lua
+++ vim-lua/config/keymaps.lua
@@ -6,6 +6,3 @@ vim.g.mapleader = ' '
 
 options = { noremap = true }
 map('n', '<Leader><ESC>', ':nohlsearch<CR>', options)
-
--- Reload the vim-lua configuration
-map('n', '<Leader>ü', ':source ~/config/vim-lua/init.lua<CR>', options)
--- vim-lua/plugins/fuzzy.lua
+++ vim-lua/plugins/fuzzy.lua
@@ -10,10 +10,26 @@ return function(use)
       local builtin = require('telescope.builtin')
 
       vim.keymap.set('n', '<Leader>e', builtin.find_files, {})
-      vim.keymap.set('n', '<Leader>f', builtin.live_grep, {})
       vim.keymap.set('n', '<Leader>E', builtin.buffers, {})
+
+      vim.keymap.set('n', '<Leader>f', builtin.live_grep, {})
+      vim.keymap.set('n', '<Leader>F', builtin.grep_string, {})
+
+      vim.keymap.set('n', '<Leader>hf', builtin.oldfiles, {})
+      vim.keymap.set('n', '<Leader>he', builtin.oldfiles, {})
+      vim.keymap.set('n', '<Leader>hc', builtin.command_history, {})
+      vim.keymap.set('n', '<Leader>hs', builtin.search_history, {})
+
+      vim.keymap.set('n', '<Leader>l', builtin.current_buffer_fuzzy_find, {})
+
       vim.keymap.set('n', '<Leader>z', builtin.help_tags, {})
+      vim.keymap.set('n', '<Leader>Z', builtin.current_buffer_tags, {})
+
+      vim.keymap.set('n', '<Leader>m', builtin.marks, {})
+
       vim.keymap.set('n', '<Leader>c', builtin.commands, {})
+
+      vim.keymap.set('n', '<Leader>gh', builtin.git_status, {})
     end
     }
 end
