58b401dfeecf12ae6ddf5e05882c34957086287c
Peter Gundel <mail@petergundel.de>
2022-10-12 13:42:21 +0200

    Add nvim-surround to vim lua

--- vim-lua/plugins/other.lua
+++ vim-lua/plugins/other.lua
@@ -1,4 +1,11 @@
 return function(use)
   use { 'editorconfig/editorconfig-vim' }
   use { 'tpope/vim-commentary' }
+  use {
+    'kylechui/nvim-surround',
+    tag = "*",
+    config = function()
+      require("nvim-surround").setup({})
+    end
+  }
 end
