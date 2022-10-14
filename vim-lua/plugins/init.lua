ccc05f251d10b480326ded5af29628fe8042ae70
Peter Gundel <mail@petergundel.de>
2022-10-14 19:19:58 +0200

    Add NeoZoom

--- vim-lua/plugins/init.lua
+++ vim-lua/plugins/init.lua
@@ -15,6 +15,7 @@ function m.config(use)
     'plugins.fuzzy',
     'plugins.tests',
     'plugins.ui',
+    'plugins.zoom',
   }
 
   for _, v in pairs(modules) do
@@ -24,36 +25,3 @@ function m.config(use)
 end
 
 return m
-
--- From old setup. To be deleted eventually.
-
--- local vim = vim
--- local execute = vim.api.nvim_command
--- local fn = vim.fn
---
--- -- ensure that packer is installed
--- local install_path = fn.stdpath('data')..'/site/pack/packer/opt/packer.nvim'
---
--- if fn.empty(fn.glob(install_path)) > 0 then
---     execute('!git clone https://github.com/wbthomason/packer.nvim '..install_path)
---     execute 'packadd packer.nvim'
--- end
---
--- vim.cmd('packadd packer.nvim')
---
--- local packer = require'packer'
--- local util = require'packer.util'
---
--- packer.init({
---   package_root = util.join_paths(vim.fn.stdpath('data'), 'site', 'pack')
--- })
---
--- -- packer startup and plugin configuration
--- packer.startup(function(use)
---   use { 'wbthomason/packer.nvim' }
---
---   -- Syntax highlighting
---   use { 'base16-project/base16-vim', config = [[require('config.base16-vim')]] }
---   use { 'nvim-treesitter/nvim-treesitter', config = [[require('config.nvim-treesitter')]] }
---   -- use { 'sheerun/vim-polyglot' }
--- end)
