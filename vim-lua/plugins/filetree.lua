e02d7a6f0ec096572ac55c8c1a4e9e203f6d4b6d
Peter Gundel <mail@petergundel.de>
2022-10-24 07:14:44 +0200

    Enable floating window in nvim-tree

--- vim-lua/plugins/filetree.lua
+++ vim-lua/plugins/filetree.lua
@@ -5,7 +5,21 @@ return function(use)
       'nvim-tree/nvim-web-devicons',
     },
     config = function()
-      require("nvim-tree").setup()
+      require("nvim-tree").setup({
+        view = {
+          float = {
+            enable = true,
+            open_win_config = {
+              relative = "editor",
+              border = "rounded",
+              width = 100,
+              height = 60,
+              row = 1,
+              col = 75,
+            },
+          },
+        },
+      })
 
       vim.keymap.set('n', '<Leader>n', ":NvimTreeToggle<CR>", { noremap = true })
       vim.keymap.set('n', '<Leader>N', ":NvimTreeFindFile<CR>", { noremap = true })
