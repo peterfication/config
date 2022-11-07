ee4bf05ffd754604554658f8004fed53addf1258
Peter Gundel <mail@petergundel.de>
2022-11-07 08:06:08 +0100

    Add nvim todo comments for better hightlights

--- vim-lua/plugins/ui.lua
+++ vim-lua/plugins/ui.lua
@@ -227,4 +227,16 @@ return function(use)
       })
     end
   }
+
+  use {
+    "folke/todo-comments.nvim",
+    requires = "nvim-lua/plenary.nvim",
+    config = function()
+      require("todo-comments").setup {
+        -- your configuration comes here
+        -- or leave it empty to use the default settings
+        -- refer to the configuration section below
+      }
+    end
+  }
 end
