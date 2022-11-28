5de1c1e4b6e0f216345f6ded45410f328e4b861f
Peter Gundel <mail@petergundel.de>
2022-11-28 09:39:32 +0100

    Update tmux conf for terminal colors fix

--- zsh/05_tmux.zsh
+++ zsh/05_tmux.zsh
@@ -1,4 +1,4 @@
-alias tm='tmux -2'
+alias tm='env TERM=screen-256color tmux -2'
 
 if ! [ -f ~/.tmux/plugins/tpm/tpm ]
 then
@@ -18,23 +18,23 @@ function tmux_new_with_name() {
     DIR_NAME=`echo $TS_DIR | grep -o "[a-zA-Z0-9_.-]*$" | sed -r 's/[.]+/-/g'`
     echo "Create session '${DIR_NAME}' with window 'vim' and folder '${TS_DIR}'"
     eval "tmux new-session -d -c ${TS_DIR} -s ${DIR_NAME} -n vim"
-    eval "tmux send-keys -t 1.0 vim Enter"
+    eval "tmux send-keys -t 1.0 vimnightly Enter"
 
     # echo "Split window 'console' in session '${DIR_NAME}'"
     # eval "tmux split-window -h -t ${DIR_NAME} -c ${TS_DIR}"
 
-    echo "Create window 'console' in session '${DIR_NAME}' with folder '${TS_DIR}'"
-    eval "tmux new-window -t ${DIR_NAME}: -c ${TS_DIR} -n console"
+    # echo "Create window 'console' in session '${DIR_NAME}' with folder '${TS_DIR}'"
+    # eval "tm new-window -t ${DIR_NAME}: -c ${TS_DIR} -n console"
 
     # Show vim window
-    eval "tmux next-window"
+    # eval "tm next-window"
   done
 }
 
 function ts() {
   tmux_new_with_name `pwd`
   DIR_NAME=`pwd | grep -o "[a-zA-Z0-9._-]*$" | sed -r 's/[.]+/-/g'`
-  tmux -2 a -t $DIR_NAME
+  tm a -t $DIR_NAME
 }
 
 # This function is for tmux session creations. See tmux/04_mappings.tmux
