4f73af7c9f7518e1e04361cfd0da14be3dece1ca
Peter Gundel <mail@petergundel.de>
2022-12-01 09:27:57 +0100

    Add neotest-jest

--- vim-lua/plugins/tests.lua
+++ vim-lua/plugins/tests.lua
@@ -6,11 +6,20 @@ return function(use)
       'nvim-treesitter/nvim-treesitter',
       'antoinemadec/FixCursorHold.nvim',
       'olimorris/neotest-rspec',
+      'haydenmeade/neotest-jest',
     },
     config = function()
       require('neotest').setup({
         adapters = {
           require('neotest-rspec'),
+          require('neotest-jest')({
+            jestCommand = "npm test --",
+            jestConfigFile = "custom.jest.config.ts",
+            env = { CI = true },
+            cwd = function(path)
+              return vim.fn.getcwd()
+            end,
+          }),
         },
         icons = {
           child_indent = "│",
