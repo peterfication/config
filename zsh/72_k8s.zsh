208be2f575ed79dbc74ea3f99fb7c94729142ea8
Peter Gundel <mail@petergundel.de>
2023-02-09 08:20:56 +0100

    Add k8s-proxy-eks-dashboard zsh function

--- zsh/72_k8s.zsh
+++ zsh/72_k8s.zsh
@@ -11,3 +11,9 @@ fi
 # kube-context-switch () {
 #     kubectl config get-contexts | tail -n +2 | fzf | cut -c 2- | awk ‘{print $1}’ | xargs kubectl config use-context
 #  }
+
+k8s-proxy-eks-dashboard () {
+  kubectl -n kube-system get secret $(kubectl -n kube-system get secret | grep eks-admin | awk '{print $1}') -o json | jq -r '.data.token' | base64 -d | pbcopy
+  echo "http://localhost:8001/api/v1/namespaces/kubernetes-dashboard/services/https:kubernetes-dashboard:/proxy/#!/login"
+  kubectl proxy
+}
