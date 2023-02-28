6d88d9877cb9c0bf408c35078a15179d2d0640a1
Peter Gundel <mail@petergundel.de>
2023-02-28 13:01:48 +0100

    Update terminal keymap

--- vim-lua/plugins/terminal.lua
+++ vim-lua/plugins/terminal.lua
@@ -13,6 +13,15 @@ return function(use)
       -- NOTE: <Leader><ESC> would be a nice mapping but this will mess with spaces that
       -- that you want to enter.
       vim.keymap.set('t', '<C-e>', [[<C-\><C-n>]], { noremap = true })
+      vim.keymap.set('t', '<C-Space>', [[<C-\><C-n>]], { noremap = true })
+      vim.keymap.set('n', '<Leader>t1', '<CMD>ToggleTerm 1<CR>', { noremap = true })
+      vim.keymap.set('n', '<Leader>t2', '<CMD>ToggleTerm 2<CR>', { noremap = true })
+      vim.keymap.set('n', '<Leader>t4', '<CMD>ToggleTerm 3<CR>', { noremap = true })
+      vim.keymap.set('n', '<Leader>t5', '<CMD>ToggleTerm 3<CR>', { noremap = true })
+      vim.keymap.set('n', '<Leader>t6', '<CMD>ToggleTerm 3<CR>', { noremap = true })
+      vim.keymap.set('n', '<Leader>t7', '<CMD>ToggleTerm 3<CR>', { noremap = true })
+      vim.keymap.set('n', '<Leader>t8', '<CMD>ToggleTerm 3<CR>', { noremap = true })
+      vim.keymap.set('n', '<Leader>t9', '<CMD>ToggleTerm 3<CR>', { noremap = true })
     end
   }
 end
