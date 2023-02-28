4e1666c88c5f81ba644e33cb1ac6f28da00ad8e4
Peter Gundel <mail@petergundel.de>
2023-02-28 13:04:38 +0100

    Alias k9s with kns
    ... for easier writing.

--- zsh/72_k8s.zsh
+++ zsh/72_k8s.zsh
@@ -17,3 +17,5 @@ k8s-proxy-eks-dashboard () {
   echo "http://localhost:8001/api/v1/namespaces/kubernetes-dashboard/services/https:kubernetes-dashboard:/proxy/#!/login"
   kubectl proxy
 }
+
+alias kns=k9s
