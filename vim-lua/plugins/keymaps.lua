4f141ff8f8a12e2d2ee7c2be21550a7e9cfcc26d
Peter Gundel <mail@petergundel.de>
2023-03-28 12:31:44 +0200

    Add nvim-spider

--- vim-lua/plugins/keymaps.lua
+++ vim-lua/plugins/keymaps.lua
@@ -16,6 +16,29 @@ return function(use)
       vim.o.timeout = true
       vim.o.timeoutlen = 300
       require("which-key").setup({})
+
+    end,
+  })
+
+  use({
+    "chrisgrieser/nvim-spider",
+    config = function()
+      -- Only set it for normal mode, so that when changing a word, it doesn't
+      -- interfere with the normal mode keybindings. Alternatively, it could be set to
+      -- { "n", "o", "x" } but then changing some.thing to other.thing wouldn't be possible
+      -- with cw anymore.
+      vim.keymap.set({ "n" }, "w", function()
+        require("spider").motion("w")
+      end, { desc = "Spider-w" })
+      vim.keymap.set({ "n" }, "e", function()
+        require("spider").motion("e")
+      end, { desc = "Spider-e" })
+      vim.keymap.set({ "n" }, "b", function()
+        require("spider").motion("b")
+      end, { desc = "Spider-b" })
+      vim.keymap.set({ "n" }, "ge", function()
+        require("spider").motion("ge")
+      end, { desc = "Spider-ge" })
     end,
   })
 end
