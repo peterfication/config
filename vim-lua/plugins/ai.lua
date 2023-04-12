776e0f0d9b49fbfac9e6082b4a1ecb6f0154b617
Peter Gundel <mail@petergundel.de>
2023-04-12 08:11:37 +0200

    Change from copilot.vim to copilot.lua

--- vim-lua/plugins/ai.lua
+++ vim-lua/plugins/ai.lua
@@ -1,5 +1,28 @@
 return function(use)
-  use({ "github/copilot.vim" })
+  -- use({ "github/copilot.vim" })
+  use({
+    "zbirenbaum/copilot.lua",
+    cmd = "Copilot",
+    event = "InsertEnter",
+    config = function()
+      require("copilot").setup({
+        suggestion = {
+          enabled = false,
+        },
+        panel = {
+          enabled = false,
+        },
+      })
+    end,
+  })
+
+  use({
+    "zbirenbaum/copilot-cmp",
+    after = { "copilot.lua" },
+    config = function()
+      require("copilot_cmp").setup()
+    end,
+  })
 
   use({
     "jackMort/ChatGPT.nvim",
--- vim-lua/plugins/auto-completion.lua
+++ vim-lua/plugins/auto-completion.lua
@@ -7,6 +7,7 @@ return function(use)
       { "hrsh7th/cmp-buffer" },
       { "hrsh7th/cmp-path" },
       { "hrsh7th/cmp-cmdline" },
+      { "zbirenbaum/copilot-cmp" },
     },
     after = "LuaSnip",
     config = function()
@@ -39,6 +40,7 @@ return function(use)
         }),
         -- https://github.com/hrsh7th/nvim-cmp/wiki/List-of-sources
         sources = cmp.config.sources({
+          { name = "copilot" },
           { name = "nvim_lsp" },
           { name = "luasnip" }, -- For luasnip users.
         }, {
@@ -46,6 +48,24 @@ return function(use)
           { name = "path" },
           -- { name = 'cmdline' },
         }),
+        sorting = {
+          priority_weight = 2,
+          comparators = {
+            -- require("copilot_cmp.comparators").prioritize,
+
+            -- Below is the default comparator list and order for nvim-cmp
+            cmp.config.compare.offset,
+            -- cmp.config.compare.scopes, --this is commented in nvim-cmp too
+            cmp.config.compare.exact,
+            cmp.config.compare.score,
+            cmp.config.compare.recently_used,
+            cmp.config.compare.locality,
+            cmp.config.compare.kind,
+            cmp.config.compare.sort_text,
+            cmp.config.compare.length,
+            cmp.config.compare.order,
+          },
+        },
       })
 
       -- Set configuration for specific filetype.
