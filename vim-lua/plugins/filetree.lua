0045c781b2533df76d3c0b6c611db6e11b5c27df
Peter Morgenstern <mail@petergundel.de>
2023-08-16 14:09:42 +0200

    Use NeoTree instead of NvimTree

--- vim-lua/plugins/filetree.lua
+++ vim-lua/plugins/filetree.lua
@@ -4,20 +4,39 @@ return function(use)
     branch = "v3.x",
     requires = {
       "nvim-lua/plenary.nvim",
-      "nvim-tree/nvim-web-devicons",
       "MunifTanjim/nui.nvim",
       "folke/which-key.nvim",
     },
     config = function()
+      require("neo-tree").setup({
+        filesystem = {
+          window = {
+            mappings = {
+              ["o"] = "system_open",
+            },
+          },
+          commands = {
+            system_open = function(state)
+              local node = state.tree:get_node()
+              local path = node:get_id()
+              path = vim.fn.shellescape(path)
+              -- macOs: open file in default application in the background.
+              vim.api.nvim_command("silent !open " .. path)
+              -- Linux: open file in default application
+              -- vim.api.nvim_command("silent !xdg-open " .. path)
+            end,
+          },
+        },
+      })
       require("which-key").register({
         ["<Leader>"] = {
           n = {
-            ":Neotree toggle<CR>",
-            "Open NvimTree in last state",
+            ":Neotree toggle float<CR>",
+            "Open NeoTree in last state",
           },
           N = {
-            ":Neotree filesystem reveal<CR>",
-            "Open NvimTree with the current file selected",
+            ":Neotree filesystem reveal float<CR>",
+            "Open NeoTree with the current file selected",
           },
         },
       })
