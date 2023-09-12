a5f67c1c555952e82b9500d175fba52acd940166
Peter Morgenstern <mail@petergundel.de>
2023-09-12 11:21:42 +0200

    Migrate from nvim packer to lazy

--- vim-lua/init.lua
+++ vim-lua/init.lua
@@ -1,36 +1,20 @@
--- Install packer
-local install_path = vim.fn.stdpath("data") .. "/site/pack/packer/start/packer.nvim"
-local is_bootstrap = false
-if vim.fn.empty(vim.fn.glob(install_path)) > 0 then
-  is_bootstrap = true
-  vim.fn.execute("!git clone https://github.com/wbthomason/packer.nvim " .. install_path)
-  vim.cmd([[packadd packer.nvim]])
+local lazypath = vim.fn.stdpath("data") .. "/lazy/lazy.nvim"
+if not vim.loop.fs_stat(lazypath) then
+  vim.fn.system({
+    "git",
+    "clone",
+    "--filter=blob:none",
+    "https://github.com/folke/lazy.nvim.git",
+    "--branch=stable", -- latest stable release
+    lazypath,
+  })
 end
+vim.opt.rtp:prepend(lazypath)
 
--- During bootstrap, only install the plugins
-if is_bootstrap then
-  require("plugins.packer-config")
-  require("packer").sync()
-
-  print("==================================")
-  print("    Plugins are being installed")
-  print("    Wait until Packer completes,")
-  print("       then restart nvim")
-  print("==================================")
-
-  return
-end
-
-local modules = {
-  "config.reload",
-  "config.settings",
-  "config.keymaps",
-  "plugins.packer-config",
-}
-
-for _, v in pairs(modules) do
-  package.loaded[v] = nil
-  require(v)
-end
-
-vim.cmd("source ~/config/vim-lua/config/folding.vim")
+require("config.options")
+require("config.keymaps")
+require("lazy").setup({
+  spec = {
+    { import = "plugins" },
+  },
+})
--- zsh/12_neovim.zsh
+++ zsh/12_neovim.zsh
@@ -3,9 +3,8 @@
 # git clone git@github.com:NvChad/NvChad ~/.config/nvim-chad
 # git clone git@github.com:SpaceVim/SpaceVim.git ~/.config/nvim-space
 # ln -s ~/config/nvim-lazy ~/.config/nvim-lazy
-# ln -s ~/config/vim-lua-2 ~/.config/nvim-lua-2
+# ln -s ~/config/vim-lua ~/.config/nvim
 
-alias nvim2="NVIM_APPNAME=nvim-lua-2 nvim"
 alias nvim-none="NVIM_APPNAME=nvim-none nvim"
 alias nvim-astro="NVIM_APPNAME=nvim-astro nvim"
 alias nvim-chad="NVIM_APPNAME=nvim-chad nvim"
