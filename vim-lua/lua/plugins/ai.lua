9a56b41cce11110f5e10f6bb2dabf803df89107f
Peter Morgenstern <mail@petergundel.de>
2023-10-19 09:44:00 +0200

    Lazy init nvim ChatGPT

--- vim-lua/lua/plugins/ai.lua
+++ vim-lua/lua/plugins/ai.lua
@@ -19,7 +19,7 @@ return {
   {
     "zbirenbaum/copilot-cmp",
     dependencies = {
-      "copilot.lua"
+      "copilot.lua",
     },
     config = function()
       require("copilot_cmp").setup()
@@ -29,29 +29,36 @@ return {
   {
     "jackMort/ChatGPT.nvim",
     config = function()
-      require("chatgpt").setup({
-        api_key_cmd = "op read op://Freelancing/6ej4zcr22njqjyjfwcofva7sru/api_key",
-        keymaps = {
-          close = { "<C-c>" },
-          submit = "<C-g>",
-          yank_last = "<C-y>",
-          yank_last_code = "<C-k>",
-          scroll_up = "<C-u>",
-          scroll_down = "<C-d>",
-          toggle_settings = "<C-o>",
-          new_session = "<C-n>",
-          cycle_windows = "<Tab>",
-          -- in the Sessions pane
-          select_session = "<Space>",
-          rename_session = "r",
-          delete_session = "d",
-        },
+      require("legendary").command({
+        ":ChatGPTInit",
+        function()
+          require("chatgpt").setup({
+            api_key_cmd = "op read op://Freelancing/6ej4zcr22njqjyjfwcofva7sru/api_key",
+            keymaps = {
+              close = { "<C-c>" },
+              submit = "<C-g>",
+              yank_last = "<C-y>",
+              yank_last_code = "<C-k>",
+              scroll_up = "<C-u>",
+              scroll_down = "<C-d>",
+              toggle_settings = "<C-o>",
+              new_session = "<C-n>",
+              cycle_windows = "<Tab>",
+              -- in the Sessions pane
+              select_session = "<Space>",
+              rename_session = "r",
+              delete_session = "d",
+            },
+          })
+        end,
+        description = "Initialize ChatGPT",
       })
     end,
     requires = {
       "MunifTanjim/nui.nvim",
       "nvim-lua/plenary.nvim",
       "nvim-telescope/telescope.nvim",
+      "mrjones2014/legendary.nvim",
     },
   },
 }
