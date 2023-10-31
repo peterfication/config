2278072498ac82bf5435d9a6b33878b4ba548319
Peter Morgenstern <mail@petergundel.de>
2023-10-31 07:07:38 +0100

    Change from null-ls to none-ls

--- vim-lua/lua/plugins/lsp.lua
+++ vim-lua/lua/plugins/lsp.lua
@@ -372,7 +372,7 @@ return {
   },
 
   {
-    "jose-elias-alvarez/null-ls.nvim",
+    "nvimtools/none-ls.nvim",
     dependencies = {
       "nvim-lua/plenary.nvim",
       "mrjones2014/legendary.nvim",
