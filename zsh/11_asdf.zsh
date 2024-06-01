fdea69fe5be500eb2ec670cbc4be73d02dfc8bbe
Peter Morgenstern <mail@petergundel.de>
2024-06-01 18:35:20 +0200

    Use mise instead of asdf

--- zsh/11_asdf.zsh
+++ zsh/11_asdf.zsh
@@ -1,5 +1,16 @@
-. $(brew --prefix)/opt/asdf/libexec/asdf.sh
-export PATH=$PATH:$HOME/.asdf/shims
+# . $(brew --prefix)/opt/asdf/libexec/asdf.sh
+# export PATH=$PATH:$HOME/.asdf/shims
+eval "$(mise activate zsh)"
+
+# Manage kubectl versions with asdf:
+# asdf plugin-add kubectl
+# asdf install kubectl 1.25.0
+# asdf  kubectl 1.25.0
+# asdf global kubectl 1.25.0
+#
+# .tools-version file:
+# kubectl 1.25.0
+
 
 # Manage kubectl versions with asdf:
 # asdf plugin-add kubectl
