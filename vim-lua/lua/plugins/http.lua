3d90c71bad58535edb67327e5436d3baf91d717f
Peter Morgenstern <mail@petergundel.de>
2024-07-05 10:57:03 +0200

    Disable rest.nvim

--- vim-lua/lua/plugins/http.lua
+++ vim-lua/lua/plugins/http.lua
@@ -5,38 +5,39 @@ return {
     priority = 1000,
     config = true,
   },
-  {
-    "rest-nvim/rest.nvim",
-    dependencies = {
-      "nvim-lua/plenary.nvim",
-      "folke/which-key.nvim",
-      "luarocks.nvim",
-    },
-    ft = "http",
-    config = function()
-      require("rest-nvim").setup({
-        -- Highlight request on run
-        -- result = {
-        --   behavior = {
-        --     formatters = {
-        --       vnd = "jq",
-        --     },
-        --   },
-        -- },
-      })
-
-      require("which-key").register({
-        ["<Leader>"] = {
-          r = {
-            name = "REST client",
-            r = { "<Plug>RestNvim", "Run the REST request under the cursor" },
-            p = { "<Plug>RestPreviewNvim", "Preview the REST request under the cursor" },
-          },
-        },
-      })
-
-      -- vim.cmd("command! RestNvim lua require('rest-nvim').run()")
-      -- vim.cmd("command! RestNvimPreview lua require('rest-nvim').run(true)")
-    end,
-  },
+  -- Disable for now as it is not maintained anymore
+  -- {
+  --   "rest-nvim/rest.nvim",
+  --   dependencies = {
+  --     "nvim-lua/plenary.nvim",
+  --     "folke/which-key.nvim",
+  --     "luarocks.nvim",
+  --   },
+  --   ft = "http",
+  --   config = function()
+  --     require("rest-nvim").setup({
+  --       -- Highlight request on run
+  --       -- result = {
+  --       --   behavior = {
+  --       --     formatters = {
+  --       --       vnd = "jq",
+  --       --     },
+  --       --   },
+  --       -- },
+  --     })
+  --
+  --     require("which-key").register({
+  --       ["<Leader>"] = {
+  --         r = {
+  --           name = "REST client",
+  --           r = { "<Plug>RestNvim", "Run the REST request under the cursor" },
+  --           p = { "<Plug>RestPreviewNvim", "Preview the REST request under the cursor" },
+  --         },
+  --       },
+  --     })
+  --
+  --     -- vim.cmd("command! RestNvim lua require('rest-nvim').run()")
+  --     -- vim.cmd("command! RestNvimPreview lua require('rest-nvim').run(true)")
+  --   end,
+  -- },
 }
