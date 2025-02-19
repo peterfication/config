556b76f7180b854a98aa3d4df8b7ba34c9743786
Peter Morgenstern <peter.morgenstern@prenode.de>
2025-02-19 16:12:37 +0100

    Add vim colorscheme dark/light toggle

--- vim-lua/lua/plugins/ui.lua
+++ vim-lua/lua/plugins/ui.lua
@@ -88,6 +88,21 @@ return {
       })
 
       vim.cmd.colorscheme("solarized")
+
+      -- Commands to change background between dark to light
+      vim.api.nvim_create_user_command("ColorschemeDark", function()
+         vim.o.background = "dark"
+      end, {})
+      vim.api.nvim_create_user_command("ColorschemeLight", function()
+         vim.o.background = "light"
+      end, {})
+      vim.api.nvim_create_user_command("ColorschemeToggle", function()
+        if vim.o.background == "dark" then
+          vim.o.background = "light"
+        else
+          vim.o.background = "dark"
+        end
+      end, {})
     end,
   },
   {
