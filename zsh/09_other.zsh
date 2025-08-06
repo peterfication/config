1f6069440290fd5bc73dc4fa5d44432d78a76d01
Peter Morgenstern <peter.morgenstern@prenode.de>
2025-08-06 15:34:58 +0200

    Add direnv auto load

--- zsh/09_other.zsh
+++ zsh/09_other.zsh
@@ -15,3 +15,8 @@ function zvm_after_init() {
 
 # Create dir and cd into it
 function ccd { mkdir -p "$1" && cd "$1" }
+
+# Load direnv if a shell is opened directly at a location where there is a .envrc file
+if [[ -f .envrc ]] && command -v direnv >/dev/null 2>&1; then
+  direnv reload
+fi
