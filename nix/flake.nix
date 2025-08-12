8213cd8948743dcd22cf15cb4472c5192fa39ee0
Peter Morgenstern <peter.morgenstern@prenode.de>
2025-08-12 13:22:32 +0200

    nix: Add rclone

--- nix/flake.nix
+++ nix/flake.nix
@@ -13,8 +13,9 @@
       # List packages installed in system profile. To search by name, run:
       # $ nix-env -qaP | grep wget
       environment.systemPackages = with pkgs; [
-        vim
+        rclone
         sshs
+        vim
       ];
 
       environment.shellAliases = {
