7060e95a81d462452fa90ce07d06657b0bd0edfc
Peter Morgenstern <mail@petergundel.de>
2025-09-16 21:20:14 +0200

    yazi: fzf hidden files

--- zsh/09_other.zsh
+++ zsh/09_other.zsh
@@ -28,7 +28,7 @@ fi
 # See https://yazi-rs.github.io/docs/quick-start#shell-wrapper
 function y() {
 	local tmp="$(mktemp -t "yazi-cwd.XXXXXX")" cwd
-	FZF_DEFAULT_COMMAND='fd --max-depth 1' yazi "$@" --cwd-file="$tmp"
+	FZF_DEFAULT_COMMAND='fd --max-depth 1 --hidden' yazi "$@" --cwd-file="$tmp"
 	IFS= read -r -d '' cwd < "$tmp"
 	[ -n "$cwd" ] && [ "$cwd" != "$PWD" ] && builtin cd -- "$cwd"
 	rm -f -- "$tmp"
